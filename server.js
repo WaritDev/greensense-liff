b7e5d2e834ab832b679b2f6cf141a18543f2f9f7
WaritDev <WaritTheProgrammer@gmail.com>
2024-11-20 20:36:26 +0700

    init project

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const port = 8888;
+
+const LINE_API_URL = "https://api.line.me/v2/bot/message/push";
+const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN
+
+app.use(express.json());
+
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${LINE_CHANNEL_ACCESS_TOKEN}`,
+};
+
+const sendMessage = async (userId, message) => {
+  const body = {
+    to: userId,
+    messages: [
+      {
+        type: "text",
+        text: message,
+      },
+    ],
+  };
+  const response = await axios.post(LINE_API_URL, body, { headers });
+  return response;
+};
+
+app.post("/send-message", async (req, res) => {
+  const { userId, message } = req.body;
+
+  try {
+    const response = await sendMessage(userId, message);
+    console.log("=== LINE log", response.data);
+    res.json({
+      message: "Message OK",
+    });
+  } catch (error) {
+    console.log("error", error.response.data);
+    res.status(400).json({
+      error: error.response,
+    });
+  }
+});
+
+app.listen(port, async () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
