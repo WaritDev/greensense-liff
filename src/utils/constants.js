909a874fbcda54689026580b44e25262fd8745cc
WaritDev <WaritTheProgrammer@gmail.com>
2024-11-23 10:52:39 +0700

    fix bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const LIFF_ID = "2006599894-yVqBz3e3";
+const LIFF_ID = "2006599894-N77RGjlj";
 const SHEET_URL= "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7WnU9CdWErLxOxWEd1JNQ-Lk6p4dKDRPiRvUDYwkYczbyu_1qJpj2jxftioyEhgLDg4vBimw2rK4d/pub?output=csv";
 const TIMEOUT_DURATION = 30000; // 30 seconds
 const REFRESH_INTERVAL = 5 * 60 * 1000; // 5 minutes
