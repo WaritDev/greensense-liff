3bccc7441166412f106cd539c340caae87e72b72
WaritDev <WaritTheProgrammer@gmail.com>
2024-11-23 21:03:40 +0700

    fix bug

---
+++ src/utils/firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCYWx9i06SUNJubzqRmMz0a4eDGxIDHjuY",
+    authDomain: "greensense-7b257.firebaseapp.com",
+    databaseURL: "https://greensense-7b257-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "greensense-7b257",
+    storageBucket: "greensense-7b257.firebasestorage.app",
+    messagingSenderId: "466569052274",
+    appId: "1:466569052274:web:77f6c889e784d6cc1402eb",
+    measurementId: "G-RJDZ5D1Y01",
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  
