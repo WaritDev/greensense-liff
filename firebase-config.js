17a3180abcb885725fdc33cac017a22181a42d4d
WaritDev <WaritTheProgrammer@gmail.com>
2024-11-23 15:33:22 +0700

    firebase database success

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,11 @@
-// firebase-config.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCYWx9i06SUNJubzqRmMz0a4eDGxIDHjuY",
   authDomain: "greensense-7b257.firebaseapp.com",
@@ -13,8 +16,6 @@ const firebaseConfig = {
   measurementId: "G-RJDZ5D1Y01"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const analytics = getAnalytics(app);
-
-export { db, analytics };
